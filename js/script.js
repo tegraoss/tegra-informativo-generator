8c61eee5f049aad93f786a44115352389f64803e
Willian Duque <willian.duque@tegra.me>
2016-07-29 17:40:50 -0300

    implementando ng-model

--- js/script.js
+++ js/script.js
@@ -1,29 +1,5 @@
-(function (angular) {
-  'use strict';
-  angular.module("email").controller('MainCtrl', ['$scope', function ($scope) {
-    var self = this;
-    this.model = {};
-    this.copy = function (id) {
-      var clipboard = new Clipboard('.copyButton', {
-        target: function (trigger) {
-          return document.getElementById('signature-container');
-        }
-      });
-    }
-    $scope.$watch(function () {
-      return self.model.name
-    }, function (v) {
-      if (!v) {
-        self.model.email = '';
-        return;
-      }
-      var parts = v.split(' ');
-      if (parts.length >= 2) {
-        self.model.email = (parts[0] + '.' + parts[parts.length - 1]).toLowerCase();
-      }
-      if (parts.length == 1) {
-        self.model.email = parts[0].toLowerCase();
-      }
-    });
-  }])
-})(angular);
+var app = angular.module('myApp', []);
+app.controller('MainCtrl', function ($scope) {
+  $scope.title = "John";
+  $scope.text = "Doe";
+});
