a32ac309e7ae5ba52b5dbc3bf705fae06e8f2193
Willian Duque <willian.duque@tegra.me>
2016-10-24 10:54:42 -0200

    Separates the informativo-generator page css from the email content and removes some comments

--- index.php
+++ index.php
@@ -44,19 +44,13 @@ $app->post('/upload', function (Request $request) use ($app, $s3) {
 } catch (Aws\Exception\S3Exception $e) {
     echo "There was an error uploading the file.\n";
 }
-
-  //move_uploaded_file($file['tmp_name'], getcwd() . '/uploaded_files/' . $file['name']);
-  //$actual_link = "http://$_SERVER[HTTP_HOST]". '/uploaded_files/' . $file['name'];
-
   return <<<XPTO
   	<script>
   	    console.log(window.opener);
   	    window.opener.document.setFile('$elementid', '$actual_link');
-  		 // window.opener.document.setFile('$actual_link');
   		window.close();
   	</script>
 XPTO;
 });
-
 $app->run();
 ?>
